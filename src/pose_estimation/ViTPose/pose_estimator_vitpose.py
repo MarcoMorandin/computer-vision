02b7a1760a9f195c482d46739edf94a195c2613a
marcomorandin <emmeemmenove@gmail.com>
2025-08-21 17:54:42 +0200

    improved with yolo weights

--- src/pose_estimation/ViTPose/pose_estimator_vitpose.py
+++ src/pose_estimation/ViTPose/pose_estimator_vitpose.py
@@ -49,7 +49,7 @@ class ViTPoseEstimator:
         # Initialize models
         self.detector = YOLO(os.path.abspath(detector_path))
         self.detector.to(self.device)
-        # self.detector.set_classes(["person"])
+        self.detector.set_classes(["person"])
 
         
         # Configure processor settings
@@ -182,7 +182,7 @@ class ViTPoseEstimator:
             First person's keypoints as flat array, or None if no detections
         """
         # Detect persons with YOLO with configuration options
-        det_results = self.detector.predict(frame, conf=confidence_threshold, verbose=False, device=self.device, classes=[0], stream=True)
+        det_results = self.detector.predict(frame, conf=confidence_threshold, verbose=False, device=self.device, stream=True)
         det = next(det_results)
         
         if det.boxes is None or det.boxes.xyxy.shape[0] == 0:
